ed3ae0e805daafb7a5fe42f7429675fdd2d8a665
Dane Nikolic <danen1995@gmail.com>
2019-08-26 16:09:33 +0200

    Updates

---
+++ precache-manifest.f6dd9c0a461158aa796e5d121d1ab810.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bfbaeaca90a7d90d3adedda6e51b2b",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "3d28c65b1c8cfa5f5460",
+    "url": "/react-weather-app/static/css/2.929fc819.chunk.css"
+  },
+  {
+    "revision": "e07aa56de827d3b273a4",
+    "url": "/react-weather-app/static/css/main.005e0eb1.chunk.css"
+  },
+  {
+    "revision": "3d28c65b1c8cfa5f5460",
+    "url": "/react-weather-app/static/js/2.3f005019.chunk.js"
+  },
+  {
+    "revision": "e07aa56de827d3b273a4",
+    "url": "/react-weather-app/static/js/main.fd1a8e62.chunk.js"
+  },
+  {
+    "revision": "654aea34005939a42790",
+    "url": "/react-weather-app/static/js/runtime~main.6b47b80c.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/react-weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-weather-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-weather-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-weather-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-weather-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-weather-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-weather-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
