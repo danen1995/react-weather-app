811a5587a0449db24506f41cc064ece15952f9ee
Dane Nikolic <danen1995@gmail.com>
2019-08-26 15:53:38 +0200

    Updates

---
+++ precache-manifest.96721fff7172bfb438fb5d7fae00c9c9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55af8ce0288c8ceb0c2a0e7c30593e5",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "dc73a2caa1396742fc6c",
+    "url": "/weather-app/static/css/2.f7176b40.chunk.css"
+  },
+  {
+    "revision": "b64d6a7d7aa03d792387",
+    "url": "/weather-app/static/css/main.bcb6e3eb.chunk.css"
+  },
+  {
+    "revision": "dc73a2caa1396742fc6c",
+    "url": "/weather-app/static/js/2.7c46e49e.chunk.js"
+  },
+  {
+    "revision": "b64d6a7d7aa03d792387",
+    "url": "/weather-app/static/js/main.1721c957.chunk.js"
+  },
+  {
+    "revision": "52387862e54d9be38456",
+    "url": "/weather-app/static/js/runtime~main.68dbe4ab.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/danen1995/react-weather-app/precache-manifest.175a117cfec9cf576eef709268db7566.js"
+  "/weather-app/precache-manifest.96721fff7172bfb438fb5d7fae00c9c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/danen1995/react-weather-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
